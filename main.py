ff4f78beba57cdecf07c16a5c8e72f17764983dc
TheGoatH1mself <anafk2712@gmail.com>
2022-12-19 18:25:24 +0000

    dot product with for loop

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-inputs = [1, 2, 3, 2.5] # could be values from the input layer or outputs from other neurons 
+# could be values from the input layer or outputs from other neurons
+inputs = [1, 2, 3, 2.5]
 
-weights1 = [0.2, 0.8, -0.5, 1.0]
-weights2 = [0.5, -0.91, 0.26, -0.5]
-weights3 = [-0.26, -0.27, 0.17, 0.87]
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
 
-bias1 = 2
-bias2 = 3
-bias3 = 0.5
+biases = [2, 3, 0.5]
 
-output = [inputs[0] * weights1[0] + inputs[1] * weights1[1] + inputs[2] * weights1[2] + inputs[3] * weights1[3] + bias1,
-          inputs[0] * weights2[0] + inputs[1] * weights2[1] + inputs[2] * weights2[2] + inputs[3] * weights2[3] + bias2,
-          inputs[0] * weights3[0] + inputs[1] * weights3[1] + inputs[2] * weights3[2] + inputs[3] * weights3[3] + bias3]
-print(output)
+layer_outputs = []
+
+for neuron_weights, neuron_bias in zip(weights, biases): # [(weight1, bias1), (weight2, bias2), (weight3, bias3)]
+    neuron_output = 0
+    for n_input, weight in zip(inputs, neuron_weights): # [(input1, n_w_1), (input2, n_w_2), (input3, n_w_3), (input4, n_w_4)]
+        neuron_output += n_input*weight
+    neuron_output += neuron_bias
+    layer_outputs.append(neuron_output)
+
+print(layer_outputs)
